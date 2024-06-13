b3c4b1bb493dbb30a58c2875659fcaaeb7b084f9
Enock <enocksm@outlook.com>
2024-06-13 08:35:13 -0300

    testing

--- src/main/java/com/enock/Main.java
+++ src/main/java/com/enock/Main.java
@@ -1,7 +1,27 @@
 package com.enock;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        String myApiKey = "470f9c12333147f2c4e14abe";
+        System.out.println("Digite uma moeda: \n");
+        var moeda = sc.nextLine();
+        String address = "https://v6.exchangerate-api.com/v6/" + myApiKey + "/latest/" + moeda;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .GET().build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
     }
 }
---
+++ src/main/java/com/enock/prod/BuscaHttp.java
@@ -0,0 +1,10 @@
+package com.enock.prod;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class BuscaHttp {
+
+}
